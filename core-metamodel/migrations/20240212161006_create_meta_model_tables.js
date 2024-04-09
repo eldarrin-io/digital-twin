19160e3cbb80ce39aac8733def5336fb40770ce9
Eldarrin <mortxbox@live.com>
2024-04-09 17:37:07 +0100

    Initial Commit

---
+++ core-metamodel/migrations/20240212161006_create_meta_model_tables.js
@@ -0,0 +1,51 @@
+const { Knex } = require("knex");
+
+exports.up = async function(knex) {
+  // reference tables
+
+  // data tables
+  await knex.schema.createTable('ecosystem', table => {
+    table.increments('id').primary();
+    table.text('name');
+    table.text('company_name');
+  });
+
+  await knex.schema.createTable('business_capability', table => {
+    table.increments('id').primary();
+    table.text('name');
+    table.integer('ecosystem_id').references('ecosystem.id');
+  });
+
+  await knex.schema.createTableLike('business_service', 'business_capability');
+  await knex.schema.alterTable('business_service', function (table) {
+    table.integer('business_capability_id').references('business_capability.id');
+  });
+
+  await knex.schema.createTableLike('application_service', 'business_capability');
+  await knex.schema.alterTable('application_service', function (table) {
+    table.integer('business_service_id').references('business_service.id');
+  });
+
+  await knex.schema.createTableLike('application_component', 'business_capability');
+  await knex.schema.alterTable('application_component', function (table) {
+    table.integer('application_service_id').references('application_service.id');
+  });
+
+  await knex.schema.createTableLike('application_implementation', 'business_capability');
+  return  knex.schema.alterTable('application_implementation', function (table) {
+    table.integer('application_component_id').references('application_component.id');
+  });
+
+
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTable('application_implementation');
+  await knex.schema.dropTable('application_component');
+  await knex.schema.dropTable('application_service');
+  await knex.schema.dropTable('business_service');
+  await knex.schema.dropTable('business_capability');
+  return knex.schema.dropTable('ecosystem');
+
+};
+
